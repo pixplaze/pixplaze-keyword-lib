48141459f21078c2a877deefb8dd8e4dea086341
emberati <alex.shak55@gmail.com>
2022-03-30 19:27:15 +0300

    Удалены @Deprecated классы

--- src/main/java/com/pixplaze/keyword/Keyword.java
+++
@@ -1,5 +0,0 @@
-package com.pixplaze.keyword;
-
-public interface Keyword<T> {
-    T value();
-}
--- src/main/java/com/pixplaze/keyword/KeywordPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.pixplaze.keyword;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class KeywordPlugin extends JavaPlugin {
-
-}
